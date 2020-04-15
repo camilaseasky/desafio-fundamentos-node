31b025b266b76807b7cc562a948769815314fd59
camilaseasky <camila.seasky@gmail.com>
2020-04-15 18:19:56 -0400

    desafio-fundamentos-node

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -9,20 +15,51 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    this.balance.income = this.transactions.reduce(
+      (acumulador, transaction) => {
+        return transaction.type === 'income'
+          ? acumulador + transaction.value
+          : acumulador;
+      },
+      0,
+    );
+
+    this.balance.outcome = this.transactions.reduce(
+      (acumulador, transaction) => {
+        return transaction.type === 'outcome'
+          ? acumulador + transaction.value
+          : acumulador;
+      },
+      0,
+    );
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
